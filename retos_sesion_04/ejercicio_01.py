4d8b80dbcaab410d10e3df4424290e4b4aa146f8
Mauricio Ramirez <ramirezsalamancamauricio@gmail.com>
2025-05-17 14:47:03 -0400

    Añadir ejercicio_01.py

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,8 @@
+# Este ejercicio consiste en la creación de un triángulo rectángulo con prints y asterisos
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
